35c2335a9d79b087f561d1b03ed685ccbfecdf29
victorlinard <contact@victor-linard.fr>
2022-12-03 17:52:52 +0100

    start read camera

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+import cv2
+import threading
 
 # Create your views here.
 
 
 def dashboard(request):
     return render(request, 'dashboard.html', context={})
+
+
+class VideoCamera(object):
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
